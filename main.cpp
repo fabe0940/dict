d18a59b05c800bdbc83e718d5ed883f6d3128ef8
Mason Fabel <mason.fabel@gmail.com>
2014-04-24 12:59:45 -0700

    Implemented CLI argument handling
    Implemented basic command-line argument parsing in order to give the
    input filename to the program.
    
    Errors are printed when either invalid arguments or an invalid
    filename are passed into the application.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
+#include "hash.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	cout << "Hello, world!" << endl;
+	ifstream dictionary;
+
+	if(argc != 2) {
+		cerr << "invalid arguments" << endl;
+		cerr << "usage: " << argv[0] << " <filename>" << endl;
+	}
+
+	dictionary.open(argv[1]);
+
+	if(dictionary.fail()) {
+		cerr << "unable to open file " << argv[1] << endl;
+	}
+
+	dictionary.close();
 
 	exit(0);
 }
