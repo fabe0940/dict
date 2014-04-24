1af99c95f33cf3117c77ba20e16a94d3723be47a
Mason Fabel <mason.fabel@gmail.com>
2014-04-24 13:03:03 -0700

    Fixed exit bug
    Fixed a bug where the program does not end even when it encounters
    an error.
    
    Now the application (properly) exits upon errors.

--- main.cpp
+++ main.cpp
@@ -11,12 +11,14 @@ int main(int argc, char** argv) {
 	if(argc != 2) {
 		cerr << "invalid arguments" << endl;
 		cerr << "usage: " << argv[0] << " <filename>" << endl;
+		exit(0);
 	}
 
 	dictionary.open(argv[1]);
 
 	if(dictionary.fail()) {
 		cerr << "unable to open file " << argv[1] << endl;
+		exit(0);
 	}
 
 	dictionary.close();
