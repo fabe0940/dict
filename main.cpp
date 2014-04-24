abb9d65a3245813990820e8ec6c41dec910fcd30
Mason Fabel <mason.fabel@gmail.com>
2014-04-24 14:07:15 -0700

    Created interactive prompt
    Implemented an interactive prompt which accepts commands and acts
    upon the hash table accordingly.
    
    Implemented commands for all the features to date:
    
    * print -- prints the table
    
    * size -- prints the bucket sizes
    
    * minmax -- prints the min and max bucket sizes
    
    * search -- looks for a word in the table
    
    * help -- display help
    
    * quit -- exit the program

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+	int running;
 	ifstream dictionary;
 	string buffer;
 
@@ -31,11 +32,40 @@ int main(int argc, char** argv) {
 
 	dictionary.close();
 
-	PrintBucketSize();
-	PrintBucketMinMax();
-
-	Print(Lookup("cat"));
-	Print(Lookup("asdf"));
+	running = 1;
+	cout << "Hash Table [" << HASH_TABLE_SIZE << "]" << endl;
+	cout << "Created by Mason Fabel on 24 April 2014" << endl;
+	cout << "Enter \"help\" for a list of commands." << endl;
+	cout << "-----" << endl;
+	while(running) {
+		cout << "> ";
+		cin >> buffer;
+		if(buffer == "quit") {
+			running = 0;
+		} else if(buffer == "help") {
+			cout << "print:          print out the entire hash table"
+				 << endl;
+			cout << "size:           print out the size of the buckets"
+				 << endl;
+			cout << "minmax:         print out the minimum and maximum "
+				 << "bucket sizes" << endl;
+			cout << "search <word>:  search for <word> in the hash "
+				 << "table" << endl;
+			cout << "help:           print this help message" << endl;
+			cout << "quit:           terminate the program" << endl;
+		} else if(buffer == "print") {
+			PrintHashTable();
+		} else if(buffer == "size") {
+			PrintBucketSize();
+		} else if(buffer == "minmax") {
+			PrintBucketMinMax();
+		} else if(buffer == "search") {
+			cin >> buffer;
+			Print(Lookup(buffer.c_str()));
+		} else {
+			/* invalid command */
+		}
+	}
 
 	exit(0);
 }
